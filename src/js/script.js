558e7962e41427d6651c24330eb33891d2b1eec2
1abama1 <you@example.com>
2022-05-19 10:10:33 +0600

    added file readme.md

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 7,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+});
